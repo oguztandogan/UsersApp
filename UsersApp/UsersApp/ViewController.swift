31b846650a8779da007eaf3edaa265b9b5284a49
Oguz Tandogan <oguz.tandogan@gmail.com>
2023-09-02 01:29:50 +0300

    The project is created.
    - The initial project is created.
    - Main storyboard file is removed because it was unnecessary.
    - Swiftlint is implemented.

---
+++ UsersApp/UsersApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  UsersApp
+//
+//  Created by Oguz Tandogan on 2.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ UsersApp/UsersAppTests/UsersAppTests.swift
@@ -0,0 +1,22 @@
+//
+//  UsersAppTests.swift
+//  UsersAppTests
+//
+//  Created by Oguz Tandogan on 2.09.2023.
+//
+
+import XCTest
+@testable import UsersApp
+
+final class UsersAppTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+
+    func testPerformanceExample() throws {
+        self.measure {}
+    }
+}
